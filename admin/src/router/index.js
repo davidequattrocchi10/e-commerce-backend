c2c288a387f7868c39149f392ab99b1550ae1ba0
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-07-06 16:07:37 +0200

    Create a login form page and manage the possibility that an administrator may not remember the password and can regenerate it

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
+import RequestPassword from '../views/RequestPassword.vue';
+import ResetPassword from '../views/ResetPassword.vue';
 
 const routes = [
     {
@@ -12,6 +14,18 @@ const routes = [
         path: '/login',
         name: 'login',
         component: Login
+    },
+    {
+        path: '/request-password',
+        name: 'requestPassword',
+        component: RequestPassword
+    },
+    {
+        path: '/reset-password/:token',
+        // :token is a dynamic segment in the route path. This route will match any URL that starts with /reset-password/ 
+        // followed by any value (the token). Examples of matching URLs: /reset-password/abc123, /reset-password/1234.
+        name: 'resetPassword',
+        component: ResetPassword
     }
 ];
 
--- admin/src/views/ResetPassword.vue
+++ admin/src/views/ResetPassword.vue
@@ -1,49 +1,46 @@
 <template>
     <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
-                account</h2>
+
+            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Set new password
+            </h2>
         </div>
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form class="space-y-6" action="#" method="POST">
+
                 <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+                    <label for="new-password" class="block text-sm font-medium leading-6 text-gray-900">New
+                        password</label>
                     <div class="mt-2">
-                        <input id="email" name="email" type="email" autocomplete="email" required=""
+                        <input id="new-password" name="password" type="password" required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
                 <div>
                     <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+                        <label for="password-repeat" class="block text-sm font-medium leading-6 text-gray-900"> Repeat
+                            Password</label>
                     </div>
                     <div class="mt-2">
-                        <input id="password" name="password" type="password" autocomplete="current-password" required=""
+                        <input id="password-repeat" name="password_repeat" type="password" required=""
                             class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
                 </div>
 
-                <div class="flex items-center justify-between">
-                    <div class="flex items-center">
-                        <input type="checkbox" name="remember-me" id="remember-me" class="h-4 w-4 text-indigo-600">
-                        <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me</label>
-                    </div>
 
+                <div class="flex items-center justify-between\">
                     <div class="text-sm">
-                        <router-link :to="{ name: 'requestPassword' }"
-                            class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your
-                            password?</router-link>
+                        <router-link :to="{ name: 'login' }" class="font-medium text-indigo-600 hover:text-indigo-500">
+                            Go Back to Login</router-link>
                     </div>
                 </div>
-
-
 
                 <div>
                     <button type="submit"
                         class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                        Sign in
+                        Submit
                     </button>
                 </div>
             </form>
@@ -51,4 +48,6 @@
     </div>
 </template>
 
-<script></script>
+
+
+<style scoped></style>
