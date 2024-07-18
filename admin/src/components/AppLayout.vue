0573c8b773df117b198b03f6ef0a9c6d03580f00
davidequattrocchi10 <davidequattrocchi10@gmail.com>
2024-07-18 12:00:36 +0200

    Create empty layout for Authorized Users and use router-view to manage rendering components

---
+++ admin/src/components/AppLayout.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+            <div>
+                Part of the layout
+            </div>
+            <router-view></router-view>
+        </div>
+    </div>
+
+
+</template>
+
+<script setup>
+const { title } = defineProps({
+    title: String
+})
+</script>
+
+
+<style scoped></style>
